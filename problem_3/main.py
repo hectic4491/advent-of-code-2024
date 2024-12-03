be39511af3a03a7a77bbbfa7fb2b06dc4c8141e5
hectic4491 <finalrob@gmail.com>
2024-12-03 17:59:52 -0500

    Second commit

--- problem_3/main.py
+++ problem_3/main.py
@@ -1,14 +1,14 @@
 import re
 
-DATA = []
-with open("problem_3/input.txt", "r", encoding="UTF8") as f:
-    for report in f:
-        DATA.append([int(n) for n in report.split()])
 
+PATTERN = r"mul\(\d{1,3},\d{1,3}\)"
+DATA = str()
+
+with open("problem_3/input.txt", "r", encoding="UTF8") as f:
+    for line in f:
+        DATA += line
 
 # This pattern will find all necessary text strings
-pattern = r"mul\(\d{1,3},\d{1,3}\)" 
 
-pattern = re.findall(pattern, DATA)
-
-print(pattern)
+matches = re.findall(PATTERN, DATA)
+print(matches)
