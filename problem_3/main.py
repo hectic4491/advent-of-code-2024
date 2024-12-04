c06ee399ac0d6eb8d0306ac3c51559cea5495c11
hectic4491 <finalrob@gmail.com>
2024-12-03 19:11:16 -0500

    Solved part 1

--- problem_3/main.py
+++ problem_3/main.py
@@ -1,14 +1,35 @@
+# pylint: skip-file
+""" Problem 3; part 1"""
+
+# Import the RegEx library
 import re
 
-
+# This pattern will find all necessary text strings.
 PATTERN = r"mul\(\d{1,3},\d{1,3}\)"
-DATA = str()
 
+TEXT_DATA = str()
 with open("problem_3/input.txt", "r", encoding="UTF8") as f:
-    for line in f:
-        DATA += line
+  for line in f:
+    TEXT_DATA += line
 
-# This pattern will find all necessary text strings
+# Remove the excess characters on each string match
+# until we just have subarrays of 2 ints.
+INT_DATA = []
+matches = re.findall(PATTERN, TEXT_DATA)
+for string in matches:
+  string = string.replace("mul(", "")
+  string = string.replace(")", "")
+  array = string.split(",")
+  array[0] = int(array[0])
+  array[1] = int(array[1])
+  INT_DATA.append(array)
 
-matches = re.findall(PATTERN, DATA)
-print(matches)
+# Multiply the 2 ints in each subarray and add them to SOLUTION_1
+SOLUTION_1 = 0
+for subarray in INT_DATA:
+  SOLUTION_1 += subarray[0] * subarray[1]
+
+print(f"This is the solution to part 1: {SOLUTION_1}")
+
+
+""" Problem 3; part 2"""
